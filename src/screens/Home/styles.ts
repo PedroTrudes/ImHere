378e53f0cb161f75fc33f4c6ad065737c1c83c2c
PedroTrudes <pedro.jtrudes@gmail.com>
2025-05-03 12:34:36 -0300

    :recycle: refactor: changing the project structure to folders (screencs)

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'black',
+        color: '#FFF',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    containerTextTitle: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    containerTextSpan: {
+        color: '#FFF',
+        fontSize: 18
+    }
+});
